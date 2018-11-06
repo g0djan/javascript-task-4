c533865077208a1f3bbf31a8b757ecc88c67a91c
g0djan <g0dj4n@gmail.com>
2018-11-06 17:47:52 +0500

    1

--- emitter.js
+++ emitter.js
@@ -4,13 +4,15 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    const events = new Map();
+
     return {
 
         /**
@@ -18,26 +20,57 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
             console.info(event, context, handler);
+            if (!events.has(event)) {
+                events.set(event, new Map());
+            }
+            if (!events.get(event).has(context)) {
+                events.get(event).set(context, []);
+            }
+            events.get(event).get(context)
+                .push(() => handler.apply(context));
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
             console.info(event, context);
+            Array.from(events.keys()).forEach(k => {
+                if (k.startsWith(event)) {
+                    events.get(k).delete(context);
+                }
+            });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
             console.info(event);
+            const splitted = event.split('.');
+            for (let i = splitted.length; i > 0; --i) {
+                const partEvent = splitted.slice(0, i).join('.');
+                if (events.has(partEvent)) {
+                    Array.from(events.get(partEvent).entries()).forEach(([key, val]) => {
+                        val.forEach(f => f());
+                    });
+                }
+            }
+
+            return this;
         },
 
         /**
